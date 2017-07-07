503fb228e1ed2c3cc0ae4dab5cfc4317e8500814
Linards Berzins <linardsberzins@gmail.com>
2017-07-07 05:27:25 +0000

    added plan instance variables for free and paid memberships to pages controller and home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+    #Get request for / home page
     def home
+      @free_plan = Plan.find(1)
+      @paid_plan = Plan.find(2)
     end
     
     def about 
